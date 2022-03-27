9f54efe2e73830f32393b3409a2658f00ca1d988
Bradley Donahue <donahue.bradley@gmail.com>
2022-03-26 22:46:16 -0400

    Accept and merge className prop

--- src/Button.jsx
+++ src/Button.jsx
@@ -1,8 +1,8 @@
-export default function Button({ children, ...props }) {
+export default function Button({ className, children, ...props }) {
   return (
     <button
       {...props}
-      className="border border-black rounded px-4 py-2 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-sky-500 hover:bg-gray-200 disabled:border-gray-400 disabled:text-gray-400 disabled:pointer-events-none"
+      className={`border border-black rounded px-4 py-2 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-sky-500 hover:bg-gray-200 disabled:border-gray-400 disabled:text-gray-400 disabled:pointer-events-none ${className}`}
     >
       {children}
     </button>
